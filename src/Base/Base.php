67e1697c6d9dc65effb0ca3318a44849fd9008e6
Alireza Kamali <arka1373@gmail.com>
2022-06-11 10:01:26 +0430

    Follow redirects

--- src/Base/Base.php
+++ src/Base/Base.php
@@ -17,6 +17,7 @@ class Base
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_ENCODING, "");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // follow redirects
         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
         $page = curl_exec($ch);
         curl_close($ch);
