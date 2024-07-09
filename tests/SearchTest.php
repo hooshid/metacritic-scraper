8f0704fae788f25311d244de518b2d90e143f69f
a.kamali <arka1373@gmail.com>
2024-07-09 13:19:13 +0330

    Update

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -16,7 +16,6 @@ class SearchTest extends TestCase
         $this->assertEquals('black-panther', $result['results'][0]['url_slug']);
         $this->assertEquals('Black Panther', $result['results'][0]['title']);
         $this->assertEquals('After the events of Captain America: Civil War, King T’Challa returns home to the reclusive, technologically advanced African nation of Wakanda to serve as his country’s new leader. However, T’Challa soon finds that he is challenged for the throne from factions within his own country. When two foes conspire to destroy Wakanda, the hero known as Black Panther must team up with C.I.A. agent Everett K. Ross and members of the Dora Milaje, Wakanadan special forces, to prevent Wakanda from being dragged into a world war.', $result['results'][0]['description']);
-        $this->assertNull($result['results'][0]['thumbnail']);
         $this->assertEquals('2018', $result['results'][0]['year']);
         $this->assertEquals('movie', $result['results'][0]['type']);
         $this->assertGreaterThan(85, $result['results'][0]['meta_score']);
@@ -37,7 +36,6 @@ class SearchTest extends TestCase
         $this->assertEquals('game-of-thrones', $result['results'][0]['url_slug']);
         $this->assertEquals('Game of Thrones', $result['results'][0]['title']);
         $this->assertEquals('Adapted from George R.R. Martin\'s epic fantasy novel series "A Song of Ice and Fire", this series is about a fantasy world where royal houses battle for the Iron Throne.', $result['results'][0]['description']);
-        $this->assertNull($result['results'][0]['thumbnail']);
         $this->assertEquals('2011', $result['results'][0]['year']);
         $this->assertEquals('tv', $result['results'][0]['type']);
         $this->assertGreaterThan(80, $result['results'][0]['meta_score']);
